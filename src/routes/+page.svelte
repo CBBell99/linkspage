4d69627498d7f839e4ebd83db65637b354357837
CBBell99 <chrisbeckertonbell@gmail.com>
2023-02-11 17:22:07 -0800

    add links

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script></script>
+
+<h1>Linktree, but free</h1>
+
+<div>
+	<a href="https://chrisbellwebdev.vercel.app">portfolio site</a>
+</div>
+<div>
+	<a href="https://github.com/CBBell99">My Github</a>
+</div>
+<div>
+	<a href="https://www.instagram.com/humperpump">instagram</a>
+</div>
