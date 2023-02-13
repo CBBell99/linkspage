f3d7717d7c1d10836bec1e18daebdd373dfb9401
CBBell99 <chrisbeckertonbell@gmail.com>
2023-02-12 19:23:38 -0800

    remove border

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,27 @@
-<script></script>
+<style>
 
+
+  .main {
+
+    background: linear-gradient( #4ca9c1, #091e8d );
+    height: 100vh
+  }
+  .btn {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: 1px black solid;
+    border-radius:30px;
+    height: 3.5rem;
+    max-width: 40rem ;
+  }
+</style>
+
+<script></script>
+<main class="main">
 <h1>Linktree, but free</h1>
 
-<div>
+<div class="btn">
 	<a href="https://chrisbellwebdev.vercel.app">portfolio site</a>
 </div>
 <div>
@@ -11,3 +30,8 @@
 <div>
 	<a href="https://www.instagram.com/humperpump">instagram</a>
 </div>
+<div>
+  <a href="www.linkedin.com/chrisbeckertonbell">LinkedIn</a>
+</div>
+
+</main>
