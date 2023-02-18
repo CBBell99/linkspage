f3d648e526eacdc8569ce6241982b6a5c9dfcef1
CBBell99 <chrisbeckertonbell@gmail.com>
2023-02-18 11:42:32 -0800

    create button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,59 +1,55 @@
 <script>
-import IoLogoGithub from 'svelte-icons/io/IoLogoGithub.svelte'
-import IoLogoInstagram from 'svelte-icons/io/IoLogoInstagram.svelte'
-import IoLogoLinkedIn from 'svelte-icons/io/IoLogoInstagram.svelte'
-import IoLogoChrome from 'svelte-icons/io/IoLogoInstagram.svelte'
+	import IoLogoGithub from 'svelte-icons/io/IoLogoGithub.svelte';
+	import IoLogoInstagram from 'svelte-icons/io/IoLogoInstagram.svelte';
+	import IoLogoLinkedIn from 'svelte-icons/io/IoLogoLinkedIn.svelte';
+	import IoLogoChrome from 'svelte-icons/io/IoLogoChrome.svelte';
 </script>
 
 <main class="main">
 	<header>
 		<h1>Linktree, but free</h1>
 	</header>
-	<div class="btn">
-    
-		<a href="https://chrisbellwebdev.vercel.app">My Portfolio Website</a>
-	</div>
-	<div class="btn">
-		<a href="https://github.com/CBBell99">My Github Page</a>
-	</div>
-	<div class="btn">
-		<a href="https://www.instagram.com/humperpump">Instagram</a>
-	</div>
-	<div class="btn">
-		<a href="www.linkedin.com/chrisbeckertonbell">LinkedIn</a>
-	</div>
+
+	<a href=""><div></div> </a>
 </main>
 
 <style>
 	@import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap');
 
 	.main {
-		width: 100%;
+		/* flex:1; */
 		background: linear-gradient(#4ca9c1, #091e8d);
 		height: 100vh;
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-	}
-	h1 {
-		font-family: 'Inter', sans-serif;
+		max-width: 100%;
+		padding: 4rem 1rem 2rem 1rem;
 		color: white;
-	}
-	.btn {
-		display: flex;
-    justify-content: flex-start;
-		align-items: center;
-		border: 1px white solid;
-		border-radius: 30px;
-		height: 3.5rem;
-		max-width: 40rem;
-		width: 100%;
-		margin: 10px;
+		/* margin */
 	}
 
-	a {
-		text-decoration: none;
-		font-family: Inter;
+	header {
+		display: flex;
+		justify-content: center;
+	}
+
+	header > h1 {
+		font-family: 'Inter', sans-serif;
+	}
+
+	.container {
+		width: 100%;
+		margin: auto;
+	}
+	.btn {
+		width: 100%;
+		height: 3.5rem;
+		border: 1px white solid;
+		display: flex;
+		padding: 0 8px;
+	}
+
+	.icon {
 		color: white;
+		height: 2rem;
+		width: 2rem;
 	}
 </style>
